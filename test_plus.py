ca344b5ce0d523ec4f245139adb82aee2ec766fe
YoshizatoMitsuki <m-yoshizato@nec.com>
2022-09-26 15:18:41 +0900

    TEST

--- test_plus.py
+++ test_plus.py
@@ -6,4 +6,4 @@ class TestPlus(unittest.TestCase):
     def test_plus(self):
         actual_result = plus(1, 1)
     
-        self.assertEqual(actual_result, 3)
+        self.assertEqual(actual_result, 2)
