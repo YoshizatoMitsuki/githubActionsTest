f877d74ce7c07e1f5e1829b0b692cfb43a5c5c57
YoshizatoMitsuki <m-yoshizato@nec.com>
2022-09-27 13:51:26 +0900

    TEST4

--- test_plus.py
+++ test_plus.py
@@ -1,9 +1,12 @@
 import unittest
 from plus import plus
  
- 
+
 class TestPlus(unittest.TestCase):
     def test_plus(self):
         actual_result = plus(1, 1)
     
         self.assertEqual(actual_result, 2)
+
+if __name__ == '__main__':
+    unittest.main()
