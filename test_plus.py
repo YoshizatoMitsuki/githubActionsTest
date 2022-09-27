0329945472790cefd18d12a99f002d984e75549b
YoshizatoMitsuki <m-yoshizato@nec.com>
2022-09-27 14:32:25 +0900

    TEST5

--- test_plus.py
+++ test_plus.py
@@ -1,12 +1,20 @@
 import unittest
+import pytest
 from plus import plus
  
 
-class TestPlus(unittest.TestCase):
-    def test_plus(self):
-        actual_result = plus(1, 1)
+# class TestPlus(unittest.TestCase):
+#     def test_plus(self):
+#         actual_result = plus(1, 1)
     
-        self.assertEqual(actual_result, 2)
+#         self.assertEqual(actual_result, 2)
+
+
+# テストコード
+# class TestPlus():
+def test_add():
+    res = plus(1, 2)
+    assert res == 3       
 
 if __name__ == '__main__':
-    unittest.main()
+    test_add()
